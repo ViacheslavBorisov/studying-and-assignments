706ffe45d41ed63bd777a72e4d8766b81cb5b6af
Viacheslav Borisov <borisov.v.1972@gmail.com>
2022-06-14 11:58:31 +0300

    finished version of app.py - C, by person one

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 echo "New file app.py was created"
 echo "We chenged this row by VSCode" 
-<<<<<<< HEAD
-print ('Testing command: git -am "massage"...B')
-=======
-print ('Testing command: git -am "massage"...B')
->>>>>>> be43b142b7f3e6bdaca329cfc9f818a88ad0dc8b
+print ('Testing command: git -am "massage"...C')
+
 
 
 
