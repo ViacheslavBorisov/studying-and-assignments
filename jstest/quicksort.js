6b7ca4e2d71b9578a97d523aa72f63aedfd4650b
Viacheslav Borisov <borisov.v.1972@gmail.com>
2022-06-14 19:11:15 +0300

    QuickSort is OK

--- jstest/quicksort.js
+++ jstest/quicksort.js
@@ -4,9 +4,12 @@ const quickSort = (arr) => {
     if (arr.length < 2) {
         return arr;
     } else {
-        
+        const baseItem = arr[Math.floor(Math.random())*arr.length]
+        const less = arr.filter(value => value < baseItem)
+        const more = arr.filter(value => value > baseItem)
+        return [...quickSort(less), baseItem, ...quickSort(more)] 
     }
-    return; 
+   
 }
 
 console.log(quickSort(arr))
