1f7293f44dd1126c82b4af602b468d09af22ff9f
Viacheslav Borisov <borisov.v.1972@gmail.com>
2022-06-14 17:19:09 +0300

    quicksort.js was maked

---
+++ jstest/quicksort.js
@@ -0,0 +1,12 @@
+const arr = [12, 64, 152, 77, 3, 21, 875, 1, 22]
+
+const quickSort = (arr) => {
+    if (arr.length < 2) {
+        return arr;
+    } else {
+        
+    }
+    return; 
+}
+
+console.log(quickSort(arr))
